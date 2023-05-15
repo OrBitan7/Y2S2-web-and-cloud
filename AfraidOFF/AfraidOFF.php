2bb73403158510c9c06d0ab96e265f030602396e
OrBitan7 <105060615+OrBitan7@users.noreply.github.com>
2023-05-16 00:02:04 +0300

    afraidoff done

--- AfraidOFF/AfraidOFF.php
+++ AfraidOFF/AfraidOFF.php
@@ -1,3 +1,12 @@
+<?php
+if (isset($_GET['submit'])) {
+  $city = $_GET['city'];
+  $address = $_GET['Address'];
+  $time = $_GET['hour'] . ':' . $_GET['minute'] . ' ' . $_GET['amOrpm'];
+  $date = $_GET['dateInput'];
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,17 +14,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
-        crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
     <script src="js/scripts.js"></script>
-
-    <title>AfraidOFF Escorts</title>
+    <title>Schedule Confirmation</title>
 </head>
-
 <body>
     <header>
         <div class="profile-container">
@@ -33,7 +36,7 @@
                 <a class="navbar-brand delcontainer" href="#">Navigation</a>
             </div>
             <div class="container-fluid">
-                <a class="navbar-brand delcontainer" href="Escorts.html"><u>Escorts</u></a>
+                <a class="navbar-brand delcontainer" href="Escorts.html">Escorts</a>
             </div>
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">Help Others</a>
@@ -48,63 +51,49 @@
                 <a class="navbar-brand" href="https://www.eran.org.il/">“Eran” website</a>
             </div>
         </nav>
-
     </header>
     <section class="wrapper">
         <div id="logo">
             <a href="index.html">
                 <div class="logo-container"></div>
             </a>
-            <p class="Breadcrumbs">Home/Escorts</p>
+            <p class="Breadcrumbs">Home/Schedule</p>
         </div>
-        <div id="Escorts-container">
-            <div class="row justify-content-center mb-4">
-                <div class="col-4">
-                    <input type="text" class="form-control" placeholder="Search">
-                </div>
+        <div class="container mt-5">
+            <h2>Escort successfully scheduled!</h2><br>
+            <div class="col-3">
+                <button id="showConfirmation" class="btn btn-primary">Show Details</button>
             </div>
-            <div class="Escort-box">
-                <img src="images/drone.png">
-                <div class="name-and-date">
-                    <h5 class="name">Drone</h5>
-                    <h6 class="date">15.5.2023</h6>
-                </div>
-                <a href="ShowDetails.html?progId=1" class="button-link">show details</a>
-            </div>
-            <div class="Escort-box">
-                <img src="images/escort_profile_1.png">
-                <div class="name-and-date">
-                    <h5 class="name">Ronit Shvartz</h5>
-                    <h6 class="date">2.5.2023</h6>
-                </div>
-                <a href="ShowDetails.html?progId=2" class="button-link">show details</a>
-            </div>
-            <div class="Escort-box">
-                <img src="images/drone.png">
-                <div class="name-and-date">
-                    <h5 class="name">Drone</h5>
-                    <h6 class="date">20.3.2023</h6>
-                </div>
-                <a href="ShowDetails.html?progId=3" class="button-link">show details</a>
-            </div>
-            <div class="Escort-box">
-                <img src="images/escort_profile_2.png">
-                <div class="name-and-date">
-                    <h5 class="name">Ayala Hasidof</h5>
-                    <h6 class="date">1.3.2023</h6>
-                </div>
-                <a href="ShowDetails.html?progId=4" class="button-link">show details</a>
-            </div>
-            <div class="Escort-box">
-                <img src="images/escort_profile_3.png">
-                <div class="name-and-date">
-                    <h5 class="name">Amihai Bokobza</h5>
-                    <h6 class="date">20.2.2023</h6>
-                </div>
-                <a href="ShowDetails.html?progId=5" class="button-link">show details</a>
-            </div>
+        </div>
 
-
+        <div id="confirmationModal" class="modal fade" tabindex="-1" role="dialog">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Schedule Details:</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <p>City:
+                            <?php echo $city; ?>
+                        </p>
+                        <p>Address:
+                            <?php echo $address; ?>
+                        </p>
+                        <p>Time:
+                            <?php echo $time; ?>
+                        </p>
+                        <p>Date:
+                            <?php echo $date; ?>
+                        </p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
     <div class="footer">
@@ -131,6 +120,15 @@
             </div>
         </div>
     </div>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script>
+        $(document).ready(function () {
+            $('#showConfirmation').click(function () {
+                $('#confirmationModal').modal('show');
+            });
+        });
+    </script>
 </body>
 
 </html>
